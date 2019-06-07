47dc4cad697c696a11f9af1f74ea6f8cc861b3ae
Heather Zurek <heather.zurek@gmail.com>
2019-06-07 15:26:00 -0700

    delete logic to try to start over

--- src/doclookup.js
+++ src/doclookup.js
@@ -1,24 +1,19 @@
-$(document).ready(function() {
-  $('#doctorLookup').click(function() {
-    const medicalNeed = $('#need').val();
-    $('#need').val("");
+export class DoctorLookup
+{
+  findDr(drName, medIssue){
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   const getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${medicalNeed} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
-  });
-});
+}
