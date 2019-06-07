e8762e0d7a12f7c34478ea705673d5c5bfd97ed2
Heather Zurek <heather.zurek@gmail.com>
2019-06-07 13:48:41 -0700

    change logic to try and retrieve data from the api

--- src/doclookup.js
+++ src/doclookup.js
@@ -1,20 +1,24 @@
-export class DoctorLookup {
+$(document).ready(function() {
+  $('#doctorLookup').click(function() {
+    const medicalNeed = $('#need').val();
+    $('#need').val("");
 
-  export class DoctorLookup {
-      medicalCondition(input) {
-         return new Promise(function(resolve, reject) {
-           let userInput = input;
-           let request = new XMLHttpRequest();
-           let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.apiKey}`;
-           request.onload = function() {
-             if (this.status === 200) {
-               resolve(request.response);
-             } else {
-               reject(Error(request.statusText));
-             }
-           }
-           request.open("GET", url, true);
-           request.send();
-         });
-       }
-}
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${medicalNeed} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
--- src/doctorlookup-interface.js
+++ src/doctorlookup-interface.js
@@ -1,31 +1,31 @@
-import { DoctorLookup } from './doclookup.js';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-
-
-$(document).ready(function() {
-  $('#medicalCondition').click(function() {
-    let input = $('#condition').val();
-    $('#condition').val("");
-    let doctorLookup = new DoctorLookup();
-    let promise = doctorLookup.medicalCondition(input);
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let data = body.data;
-      if (data.length > 0) {
-        $('#medicalConditionSearch').html(`<p>doctors who specialize in: ${input}</p>`);
-        for (let i=0; i < data.length; i++){
-          $('.showMedicalCondition').append(`<li>${data[i].profile.first_name} ${data[i].profile.last_name}<ul><li>${data[i].practices[0].visit_address.street} ${data[i].practices[0].visit_address.city} ${data[i].practices[0].visit_address.state}</li> <li>${data[i].practices[0].phones[0].number}</li> <li>Accepts New Patients? ${data[i].practices[0].accepts_new_patients}</li></li>`);
-        }
-      } else {
-          $('#medicalConditionSearch').html(`<h4>Sorry, there are no doctors that can help with ${input}</h4>`);
-      }
-
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
-
-});
+// import { DoctorLookup } from './doclookup.js';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+//
+//
+// $(document).ready(function() {
+//   $('#medicalCondition').click(function() {
+//     let input = $('#condition').val();
+//     $('#condition').val("");
+//     let doctorLookup = new DoctorLookup();
+//     let promise = doctorLookup.medicalCondition(input);
+//     promise.then(function(response) {
+//       let body = JSON.parse(response);
+//       let data = body.data;
+//       if (data.length > 0) {
+//         $('#medicalConditionSearch').html(`<p>doctors who specialize in: ${input}</p>`);
+//         for (let i=0; i < data.length; i++){
+//           $('.showMedicalCondition').append(`<li>${data[i].profile.first_name} ${data[i].profile.last_name}<ul><li>${data[i].practices[0].visit_address.street} ${data[i].practices[0].visit_address.city} ${data[i].practices[0].visit_address.state}</li> <li>${data[i].practices[0].phones[0].number}</li> <li>Accepts New Patients? ${data[i].practices[0].accepts_new_patients}</li></li>`);
+//         }
+//       } else {
+//           $('#medicalConditionSearch').html(`<h4>Sorry, there are no doctors that can help with ${input}</h4>`);
+//       }
+//
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+//     });
+//   });
+//
+// });
