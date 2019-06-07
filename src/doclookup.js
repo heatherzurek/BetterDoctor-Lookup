e1cc29bdea6cf018decb4744d88118290c4ea114
Heather Zurek <heather.zurek@gmail.com>
2019-06-07 13:38:39 -0700

    add logic to request doctors specialties based on conditions from the api

--- src/doclookup.js
+++ src/doclookup.js
@@ -1,19 +1,20 @@
 export class DoctorLookup {
 
-  getDoctorByNeed(name, issue) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&sort=first-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`;
-'
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+  export class DoctorLookup {
+      medicalCondition(input) {
+         return new Promise(function(resolve, reject) {
+           let userInput = input;
+           let request = new XMLHttpRequest();
+           let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.apiKey}`;
+           request.onload = function() {
+             if (this.status === 200) {
+               resolve(request.response);
+             } else {
+               reject(Error(request.statusText));
+             }
+           }
+           request.open("GET", url, true);
+           request.send();
+         });
+       }
 }
