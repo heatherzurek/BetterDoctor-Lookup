b0a7130c628c319b1bca4ff74c43ef322705cb73
Heather Zurek <heather.zurek@gmail.com>
2019-06-07 15:45:50 -0700

    getting cannot post / error and 404 error

--- src/doclookup.js
+++ src/doclookup.js
@@ -3,7 +3,7 @@ export class DoctorLookup
   findDr(drName, medIssue){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
