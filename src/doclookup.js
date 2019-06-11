aa3769477ad5803d5b7ee86b295d408336553e20
Heather Zurek <heather.zurek@gmail.com>
2019-06-10 18:23:23 -0700

    add let to body, fix form input id and add es6 to the linter

--- src/doclookup.js
+++ src/doclookup.js
@@ -1,5 +1,4 @@
-export class DoctorLookup
-{
+export class DoctorLookup {
   findDoctor(doctorName){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
@@ -15,5 +14,4 @@ export class DoctorLookup
       request.send();
     });
   }
-
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
 import { DoctorLookup } from './doclookup.js';
 import $ from 'jquery';
-import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 $(document).ready(function() {
-  $('#doctorSearch').submit(function(event) {
+  $('#drSearch').submit(function(event) {
     event.preventDefault();
     $('#showDoctors').text("");
     let doctorName = $('#drname').val();
     $('#drname').val("");
 
-    let doctorLookup = new DoctorLookup();  // create instance of WeatherService class
-    let promise = doctorLookup.findDoctor(doctorName);  // call the instance method and pass in user input
+    let doctorLookup = new DoctorLookup();
+    let promise = doctorLookup.findDoctor(doctorName);
 
     promise.then(function(response) {
-      body = JSON.parse(response);
+      let body = JSON.parse(response);
       //why is body undefined???
       if(body.data.length === 0) {
-        $('#showDoctors').text(`There are no doctors in your area that match your search parameters.`)
+        $('#showDoctors').text(`There are no doctors in your area that match your search parameters.`);
       }
     });
 
   });
 });
-
-//promise is undefined.. and myu styles.css isn't showing up when I run start
